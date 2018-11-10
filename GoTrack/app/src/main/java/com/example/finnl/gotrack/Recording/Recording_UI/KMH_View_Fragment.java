fc1da0069e4409c9ec33b800cc2073f62fae7a68
Flenz01 <finnjoana56@gmail.com>
2018-11-10 19:10:29 +0100

    Konfliktbehebung

--- GoTrack/app/src/main/java/com/example/finnl/gotrack/Recording/Recording_UI/KMH_View_Fragment.java
+++ GoTrack/app/src/main/java/com/example/finnl/gotrack/Recording/Recording_UI/KMH_View_Fragment.java
@@ -26,7 +26,7 @@ public class KMH_View_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragement_record_kmh__view, container, false);
+        return inflater.inflate(R.layout.fragment_record_kmh_view, container, false);
     }
 
 }
