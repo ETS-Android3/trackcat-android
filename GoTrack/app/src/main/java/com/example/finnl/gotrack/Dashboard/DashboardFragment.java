2700ef95fdb4f557270a672789f28da47c669957
Marie Fock <fock.marie@gmail.com>
2018-11-17 14:27:34 +0100

    PageViewer für Charts

--- GoTrack/app/src/main/java/com/example/finnl/gotrack/Dashboard/DashboardFragment.java
+++ GoTrack/app/src/main/java/com/example/finnl/gotrack/Dashboard/DashboardFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import com.example.finnl.gotrack.Charts.BarChartFragment;
 import com.example.finnl.gotrack.Charts.LineChartFragment;
 import com.example.finnl.gotrack.R;
+import com.example.finnl.gotrack.Recording.Recording_UI.PageViewer;
 
 public class DashboardFragment extends Fragment {
     private FragmentTransaction fragTransaction;
@@ -19,14 +20,17 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         fragTransaction = getChildFragmentManager().beginTransaction();
 
+        fragTransaction.replace(R.id.chartContainer, new PageViewerCharts(), "PageViewer");
+/*
         // Summary dem Dashboard hinzufügen
         fragTransaction.replace(R.id.summaryContainer, new BarChartFragment(), "BAR_CHART");
 
         // LineChart dem Dashboard hinzufügen
         fragTransaction.replace(R.id.chartContainer, new LineChartFragment(), "LINE_CHART");
-        
+ */
         // Änderungen zusammenfassen + Dashboard aufbauen
         fragTransaction.commit();
+
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 }
