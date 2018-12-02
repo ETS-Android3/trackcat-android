3282aa3ceb082c211870d23886edbc77267b63fd
Timo Kramer <timokramer1@me.com>
2018-12-02 02:43:10 +0100

    Anfang Recordlist

--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/RecordList/RecordListFragment.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/RecordList/RecordListFragment.java
@@ -5,14 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import de.mobcom.group3.gotrack.Database.DAO.RouteDAO;
+import de.mobcom.group3.gotrack.Database.Models.Route;
+import de.mobcom.group3.gotrack.MainActivity;
 import de.mobcom.group3.gotrack.R;
 
 public class RecordListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_record_list, container, false);
+        RouteDAO dao = new RouteDAO(MainActivity.getInstance());
+        //TODO DYnamische Umsetzung anhand der UserID
+        List<Route> records = dao.readAll(1);
+
+        for (Route record:records) {
+            TextView recordName = view.findViewById(R.id.recordName);
+            recordName.setText(record.getName());
+
+            TextView recordDistance = view.findViewById(R.id.recordDistance);
+            recordDistance.setText(("" + record.getDistance()));
+
+            TextView recordTime = view.findViewById(R.id.recordTime);
+            recordTime.setText("" + record.getTime());
+        }
+        return view;
     }
 }
