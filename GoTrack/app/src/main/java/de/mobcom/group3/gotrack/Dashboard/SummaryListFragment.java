761508a5a3afd4ac0dec576ef32484703ec2fee8
Timo Kramer <timokramer1@me.com>
2018-12-16 17:33:32 +0100

    Wechsel zu gesamten Aufnahmen-Liste

--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Dashboard/SummaryListFragment.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Dashboard/SummaryListFragment.java
@@ -2,10 +2,14 @@ package de.mobcom.group3.gotrack.Dashboard;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 import de.mobcom.group3.gotrack.Database.DAO.RouteDAO;
@@ -13,8 +17,9 @@ import de.mobcom.group3.gotrack.Database.Models.Route;
 import de.mobcom.group3.gotrack.MainActivity;
 import de.mobcom.group3.gotrack.R;
 import de.mobcom.group3.gotrack.RecordList.CustomRecordListAdapter;
+import de.mobcom.group3.gotrack.RecordList.RecordListFragment;
 
-public class SummaryListFragment extends Fragment {
+public class SummaryListFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,13 +31,27 @@ public class SummaryListFragment extends Fragment {
         List<Route> records = dao.readAll(MainActivity.getActiveUser());
 
         List<Route> mList = new ArrayList<>();
-        for (int i = 0; i < getResources().getInteger(R.integer.summaryRecordListAmount) && i < records.size(); i++){
+        for (int i = 0; i < getResources().getInteger(R.integer.summaryRecordListAmount) && i < records.size(); i++) {
             mList.add(records.get(i));
         }
 
         CustomRecordListAdapter adapter = new CustomRecordListAdapter(MainActivity.getInstance(), mList);
         ListView recordList = view.findViewById(R.id.record_list);
         recordList.setAdapter(adapter);
+
+        LinearLayout showMore = view.findViewById(R.id.show_more_records);
+        showMore.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.show_more_records:
+                FragmentTransaction fragTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragTransaction.replace(R.id.mainFrame, new RecordListFragment(), "RECORDLIST");
+                fragTransaction.commit();
+                break;
+        }
     }
 }
