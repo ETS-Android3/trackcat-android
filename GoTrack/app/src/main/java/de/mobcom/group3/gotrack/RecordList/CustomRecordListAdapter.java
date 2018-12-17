7c863a414f0b16533878134b129160338a189808
Marie Fock <fock.marie@gmail.com>
2018-12-17 17:31:55 +0100

    Grundlagen zum Anzeigen der Detailansicht mit Charts

--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/RecordList/CustomRecordListAdapter.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/RecordList/CustomRecordListAdapter.java
@@ -2,16 +2,26 @@ package de.mobcom.group3.gotrack.RecordList;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
 import java.util.List;
 import de.mobcom.group3.gotrack.Database.Models.Route;
+import de.mobcom.group3.gotrack.MainActivity;
 import de.mobcom.group3.gotrack.R;
+import de.mobcom.group3.gotrack.Settings.NewUserFragment;
 
 public class CustomRecordListAdapter extends ArrayAdapter<String> {
 
@@ -48,6 +58,21 @@ public class CustomRecordListAdapter extends ArrayAdapter<String> {
 
         TextView recordTime = recordItem.findViewById(R.id.record_time);
         recordTime.setText(records.get(position).getTime() + " Minuten");
+
+        /* Anzeigen des Profilerstellungssfragment */
+        recordItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("id", records.get(position).getId());
+                RecordDetailsFragment recordDetailsFragment = new RecordDetailsFragment();
+                recordDetailsFragment.setArguments(bundle);
+                FragmentTransaction fragTransaction = MainActivity.getInstance().getSupportFragmentManager().beginTransaction();
+                fragTransaction.replace(R.id.mainFrame, recordDetailsFragment, "Record DETAILS");
+                fragTransaction.commit();
+                Toast.makeText(MainActivity.getInstance().getApplicationContext(), "Anzeigen der Aufnahme von ID: "+records.get(position).getId(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         return view;
     }
