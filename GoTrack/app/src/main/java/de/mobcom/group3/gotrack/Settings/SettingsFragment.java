3b122bd35595bc2a3c310c44095e354df817a953
Timo Kramer <timokramer1@me.com>
2019-01-04 14:46:38 +0100

    Feedback Mail-Einstellungen

--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Settings/SettingsFragment.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Settings/SettingsFragment.java
@@ -172,7 +172,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         }
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"timokramer1@me.com", "yannik-petersen1992@t-online.de", "fock.marie@gmail.com", "finnlenz@outlook.de", "kristoff_klan@hotmail.de", "j.petsch95@gmail.com"});
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"timokramer1@me.com", "yannik-petersen92@t-online.de", "fock.marie@gmail.com", "finnlenz@outlook.de", "kristoff_klan@hotmail.de", "j.petsch95@gmail.com"});
         intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback: GoTrack");
         intent.putExtra(Intent.EXTRA_TEXT, body);
         context.startActivity(Intent.createChooser(intent, "Wählen Sie Ihren E-Mail Client"));
