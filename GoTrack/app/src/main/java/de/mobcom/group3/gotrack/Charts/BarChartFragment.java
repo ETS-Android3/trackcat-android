aa8749865d0408218202d5c4c30170bfd276dd83
jpetsch <j.petsch95@gmail.com>
2019-01-07 16:02:49 +0100

    LineChart
    Comments
    Maybe: Fixed wrong maxVal for Range

--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Charts/BarChartFragment.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Charts/BarChartFragment.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,7 +53,7 @@ public class BarChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Checks current Theme and uses the correct xml
+        /* Checks current Theme and uses the correct xml */
         if(PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(PREF_DARK_THEME, false)) {
             view = inflater.inflate(R.layout.fragment_line_chart_dark, container, false);
         }else{
@@ -62,21 +61,21 @@ public class BarChartFragment extends Fragment {
         }
 
         if (getArguments() != null) {
-            // Gets all Arguments from Bundle
+            /* Gets all Arguments from Bundle */
             values = getArguments().getDoubleArray("array");
             title = getArguments().getString("title");
             color = getArguments().getInt("color");
             rangeTitle = getArguments().getString("rangeTitle");
             incrementStepsY = getArguments().getDouble("stepsY");
 
-            // Gets Every value from bundle in number Array
+            /* Gets Every value from bundle in number Array */
             series1Numbers = new Number[values.length];
             for (int i = 0; i < series1Numbers.length; i++) {
                 series1Numbers[i] = values[i];
             }
 
         } else {
-            // Setting default vals, if plot is not used with bundle arguments
+            /* Setting default vals, if plot is not used with bundle arguments */
             title = "Series01";
             rangeTitle="km/h";
             color = Color.GRAY;
@@ -84,24 +83,24 @@ public class BarChartFragment extends Fragment {
             series1Numbers = new Number[]{0, 1, 2, 3, 4, 5, 6, 7, 0};
         }
 
-        // Getting in xml defined Plot
+        /* Getting in xml defined Plot */
         plot = view.findViewById(R.id.linePlot);
         plot.setTitle(title);
         plot.setRangeLabel(rangeTitle);
 
-        // Turning Arrays to XYSeries
+        /* Turning Arrays to XYSeries */
         XYSeries series1 = new SimpleXYSeries(
                 Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, title);
 
-        // create a bar formatter with a fill color and outline:
+        /* create a bar formatter with a fill color and outline: */
         BarFormatter bf = new BarFormatter(color, color);
-        // Add Series of Values to Plot (Every Value is one Bar)
+        /* Add Series of Values to Plot (Every Value is one Bar) */
         plot.addSeries(series1, bf);
 
-        // Setting Start of X-Axis at 0 and End at 8
-        // Range will start at 0
-        // Steps of X-Axis are Incrementing as Natural Value from in Step by 1
-        // Graph X-Axis Label Style is customed to Weekdays
+        /*  Setting Start of X-Axis at 0 and End at 8
+            Range will start at 0
+            Steps of X-Axis are Incrementing as Natural Value from in Step by 1
+            Graph X-Axis Label Style is customed to Weekdays */
         plot.setDomainLowerBoundary(LOWER_BOUNDARY_X, BoundaryMode.FIXED);
         plot.setDomainUpperBoundary(UPPER_BOUNDARY_X, BoundaryMode.FIXED);
         plot.setRangeLowerBoundary(LOWER_BOUNDARY_Y, BoundaryMode.FIXED);
@@ -109,14 +108,14 @@ public class BarChartFragment extends Fragment {
         plot.setRangeStep(StepMode.INCREMENT_BY_VAL, incrementStepsY);
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new XLabelFormat());
 
-        // If Steps are to small to show in 0.0 Format, it will be set as 0.00 for Range
+        /* If Steps are to small to show in 0.0 Format, it will be set as 0.00 or 0.000 for Range */
         if(incrementStepsY < 0.01){
             plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("0.000"));
         }else if(incrementStepsY < 0.1){
             plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("0.00"));
         }
 
-        // Bar Width is set by plots own BarRenderer Instance
+        /* Bar Width is set by plots own BarRenderer Instance */
         BarRenderer renderer = plot.getRenderer(BarRenderer.class);
         renderer.setBarGroupWidth(BarRenderer.BarGroupWidthMode.FIXED_WIDTH, PixelUtils.dpToPix(barWidth));
 
--- GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Charts/LineChartFragment.java
+++ GoTrack/app/src/main/java/de/mobcom/group3/gotrack/Charts/LineChartFragment.java
@@ -25,7 +25,7 @@ public class LineChartFragment extends Fragment {
     private XYPlot plot;
     private int pointPerSegment = 10;
     private int incrementStepsX = 1;
-    private int incrementStepsY = 10;
+    private double incrementStepsY = 10;
     private Number[] series1Numbers;
     private double[] values;
     private String rangeTitle;
@@ -45,6 +45,7 @@ public class LineChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        /* Checks current Theme and uses the correct xml and formatter */
         if(PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(PREF_DARK_THEME, false)){
             view = inflater.inflate(R.layout.fragment_line_chart_dark, container, false);
             series1Format =
@@ -56,51 +57,52 @@ public class LineChartFragment extends Fragment {
         }
 
         if (getArguments() != null) {
+            /* Gets all Arguments from Bundle */
             title = getArguments().getString("title");
             rangeTitle = getArguments().getString("rangeTitle");
             values = getArguments().getDoubleArray("array");
         } else {
+            /* Setting default vals, if plot is not used with bundle arguments */
             title = "Series1";
             rangeTitle = "Range";
             values = new double[]{1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
         }
 
+        /* Sets every value as Value in number Array */
         series1Numbers = new Number[values.length];
-        int maxValue=0;
+        double maxValue=0;
         for (int i = 0; i < series1Numbers.length; i++) {
             series1Numbers[i] = (int) Math.round(values[i]);
-            if ((int) Math.round(values[i])> maxValue){
-                maxValue=(int) Math.round(values[i]);
+            if (maxValue < values[i]){
+                maxValue = values[i];
             }
         }
 
+        /* Incrementing Steps are created dynamically */
         pointPerSegment = series1Numbers.length;
-        incrementStepsY = maxValue/5;
+        incrementStepsY = maxValue / 5;
         incrementStepsX = series1Numbers.length/5;
 
+        /* Turning Arrays to XYSeries */
         XYSeries series1 = new SimpleXYSeries(
                 Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, title);
 
-        // Smoothing curves
+        /* Smoothing curves */
         series1Format.setInterpolationParams(
                 new CatmullRomInterpolator.Params(pointPerSegment, CatmullRomInterpolator.Type.Centripetal));
         series1Format.setPointLabelFormatter(null);
 
-        // Getting in xml defined Plot
+        /* Getting in xml defined Plot */
         plot = view.findViewById(R.id.linePlot);
         plot.setTitle(title);
         plot.setRangeLabel(rangeTitle);
 
-        // Add a new Series to the XYPlot
+        /* Add a new Series to the XYPlot */
         plot.addSeries(series1, series1Format);
 
-        // Setting Start of X-Axis at 0 and End at 8
-        // Range will start at 0
-        // Steps of X-Axis are Incrementing as Natural Value from in Step by 1
-        // Graph X-Axis Label Style is customed to Weekdays
+        /* Lower Boundaries are set to 0 as defined in final Variables */
         plot.setDomainLowerBoundary(LOWER_BOUNDARY_X, BoundaryMode.FIXED);
         plot.setRangeLowerBoundary(LOWER_BOUNDARY_Y, BoundaryMode.FIXED);
-        //TODO dynamische X-Schritte
         plot.setDomainStep(StepMode.INCREMENT_BY_VAL, incrementStepsX);
         plot.setRangeStep(StepMode.INCREMENT_BY_VAL, incrementStepsY);
 
