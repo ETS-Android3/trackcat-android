424f8d0825f6a7facefa29869bc4011fe62c81c9
Marie Fock <fock.marie@gmail.com>
2019-05-09 21:46:36 +0200

    read profile fields from profile

--- Trackcat/app/src/main/java/de/trackcat/FriendsSystem/FriendProfileFragment.java
+++ Trackcat/app/src/main/java/de/trackcat/FriendsSystem/FriendProfileFragment.java
@@ -9,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import de.trackcat.MainActivity;
 import de.trackcat.R;
 
@@ -18,6 +21,9 @@ public class FriendProfileFragment extends Fragment implements View.OnClickListe
 
     RelativeLayout loadProfile;
     Button btn_delete_friend;
+    TextView name, email, dayOfBirth, gender, amountRecords, timeRecords, dayOfRegistration;
+    CircleImageView image, state;
+    ImageView birthday, user_gender_image;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,12 +33,37 @@ public class FriendProfileFragment extends Fragment implements View.OnClickListe
 
         /* get profil fields */
         loadProfile = view.findViewById(R.id.loadScreen);
-        loadProfile.setVisibility(View.GONE);
-
+        image = view.findViewById(R.id.profile_image);
+        state = view.findViewById(R.id.profile_state);
+        name = view.findViewById(R.id.user_name);
+        email = view.findViewById(R.id.user_email);
+        dayOfBirth = view.findViewById(R.id.user_dayOfBirth);
+        birthday = view.findViewById(R.id.user_birthday);
+        gender = view.findViewById(R.id.user_gender);
+        user_gender_image = view.findViewById(R.id.user_gender_image);
+        amountRecords = view.findViewById(R.id.user_amount_records);
+        timeRecords = view.findViewById(R.id.user_time_records);
+        dayOfRegistration = view.findViewById(R.id.user_dayOfRegistration);
         btn_delete_friend = view.findViewById(R.id.btn_delete_friend);
+
+        /* set loadscreen invisible */
+        loadProfile.setVisibility(View.GONE);
 
         /* set on click listener */
         btn_delete_friend.setOnClickListener(this);
+
+        /* set values */
+        //TODO values aus der Datenbank anfragen
+       // image.setImageURI();
+      //  state = view.findViewById(R.id.profile_state);
+        name.setText("Anna Webstar");
+        email.setText("anna@web.de");
+        dayOfBirth.setText("12.12.1998");
+        gender.setText("weiblich");
+      //  user_gender_image = view.findViewById(R.id.user_gender_image);
+        amountRecords.setText("20");
+        timeRecords.setText("12:20:12");
+        dayOfRegistration.setText("13.09.2014");
 
         return view;
     }
