a2b3b50d7829ae99a849a0e4d9cedae2cde21826
Marie Fock <fock.marie@gmail.com>
2019-07-20 12:10:41 +0200

    basic for load more

--- Trackcat/app/src/main/java/de/trackcat/FriendsSystem/Tabs/FindFriendsFragment.java
+++ Trackcat/app/src/main/java/de/trackcat/FriendsSystem/Tabs/FindFriendsFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -35,25 +36,35 @@ import de.trackcat.FriendsSystem.FriendListAdapter;
 import de.trackcat.GlobalFunctions;
 import de.trackcat.MainActivity;
 import de.trackcat.R;
+import de.trackcat.SignIn.SignInFragment_1;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class FindFriendsFragment extends Fragment implements View.OnKeyListener {
+public class FindFriendsFragment extends Fragment implements View.OnKeyListener, View.OnClickListener {
 
     EditText findFriend;
     private UserDAO userDAO;
     View view;
+    Button loadMore;
+    String searchTerm;
+    int page;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_friends_find, container, false);
+        loadMore= view.findViewById(R.id.loadMore);
+        loadMore.setOnClickListener(this);
 
+        /* create user DAO */
         userDAO = new UserDAO(MainActivity.getInstance());
+
+        /* set page */
+        page=1;
 
         /* find view */
         findFriend = view.findViewById(R.id.findFriend);
@@ -61,10 +72,10 @@ public class FindFriendsFragment extends Fragment implements View.OnKeyListener 
 
         /* set last search */
         if(getArguments()!=null){
-            String searchTerm = getArguments().getString("searchTerm");
+            searchTerm = getArguments().getString("searchTerm");
             findFriend.setText(searchTerm);
             /* search term */
-            search(searchTerm);
+            search(searchTerm, page);
         }
 
 
@@ -75,28 +86,29 @@ public class FindFriendsFragment extends Fragment implements View.OnKeyListener 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
         if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-            String find = findFriend.getText().toString();
-            Toast.makeText(getContext(), "Suche nach '" + find + "' gestartet.", Toast.LENGTH_SHORT).show();
+            searchTerm = findFriend.getText().toString();
+            page=1;
+            Toast.makeText(getContext(), "Suche nach '" + searchTerm + "' gestartet.", Toast.LENGTH_SHORT).show();
 
             InputMethodManager imm = (InputMethodManager)MainActivity.getInstance().getSystemService(Context.INPUT_METHOD_SERVICE);
             View view = MainActivity.getInstance().getCurrentFocus();
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
 
             /* search term */
-            search(find);
+            search(searchTerm, page);
 
             return true;
         }
         return false;
     }
 
-    private void search(String find){
+    private void search(String find, int page){
         /* set gloabl value */
         MainActivity.setSearchTerm(find);
 
         HashMap<String, String> map = new HashMap<>();
         map.put("search", "" + find);
-        map.put("page", "1");
+        map.put("page", ""+page);
 
         Retrofit retrofit = APIConnector.getRetrofit();
         APIClient apiInterface = retrofit.create(APIClient.class);
@@ -138,6 +150,14 @@ public class FindFriendsFragment extends Fragment implements View.OnKeyListener 
                     ListView friendListView = view.findViewById(R.id.friend_list);
                     friendListView.setAdapter(adapter);
 
+                    /* set show more button visible/gone */
+                    if(friendList.size()==10){
+                        loadMore.setVisibility(View.VISIBLE);
+                    }else{
+                        loadMore.setVisibility(View.GONE);
+                    }
+
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
@@ -151,5 +171,18 @@ public class FindFriendsFragment extends Fragment implements View.OnKeyListener 
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.loadMore:
+                page++;
+                /* search term */
+                search(searchTerm, page);
+                break;
+
+        }
     }
 }
