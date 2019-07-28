d427a0ee319664e92e899a3c51e174eaf4e77466
Marie Fock <fock.marie@gmail.com>
2019-07-28 10:27:05 +0200

    change logo to svg

--- Trackcat/app/src/main/java/de/trackcat/GlobalFunctions.java
+++ Trackcat/app/src/main/java/de/trackcat/GlobalFunctions.java
@@ -331,7 +331,7 @@ public class GlobalFunctions {
         String base = currentUser.getMail() + ":" + currentUser.getPassword();
         String authString = "Basic " + Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
         Call<ResponseBody> call = apiInterface.abortLiveRecord(authString);
-
+        Log.d("GESCHLOSSEN","base: "+base);
         call.enqueue(new Callback<ResponseBody>() {
 
             @Override
